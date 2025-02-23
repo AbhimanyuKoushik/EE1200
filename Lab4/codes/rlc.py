6667f755f8d7ef14d0d034979a0d916f54dc97d3
agamjotsingh1 <agamjot.s.kohli@gmail.com>
2025-02-23 12:03:00 +0530

    Lab4 init

--- Lab4/codes/rlc.py
+++ Lab4/codes/rlc.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import math as m
 
 # Circuit parameters
-R = 23   # Resistance in ohms
-C = 1e-4   # Capacitance in farads
+R = 24.5   # Resistance in ohms
+C = 460e-12   # Capacitance in farads
 L = 2.2e-3   # Inductance in henries
 Vc0 = 5           # Initial capacitor voltage
 
@@ -13,8 +13,8 @@ b = 1/(L*C)
 disc = (a*a/4) - b
 
 # Simulation parameters
-stepsize = 0.0001  # Step size
-tmax = 0.01    # Maximum simulation time
+stepsize = 1e-9
+tmax = 2e-4    # Maximum simulation time
 
 size = int(tmax / stepsize)
 
@@ -22,7 +22,7 @@ t = np.linspace(0, tmax, size)
 
 if disc < 0:
     w = np.sqrt(-disc)
-    Vc = Vc0*(np.e**(-a*t/2))*(np.cos(w*t) + (a/(2*w))*(np.sin(w*t)))
+    Vc = Vc0*(np.exp(-a*t/2))*(np.cos(w*t) + (a/(2*w))*(np.sin(w*t)))
 elif m.fabs(disc) <= 1e-5:
     Vc = Vc0*(1+a*t/2)*(np.e**(-a*t/2))
 elif disc > 0:
@@ -35,6 +35,7 @@ elif disc > 0:
 
 # Plot results
 plt.plot(t, Vc)
+plt.savefig("../figs/rlc.png")
 plt.xlabel("Time (s)")
 plt.ylabel("Voltage across capacitor (Vc)")
 plt.title("Current vs. Time in an RLC Circuit")
